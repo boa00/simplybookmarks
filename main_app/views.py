5329d144f903763a06c94a043a42c4dbd35c6080
boa00 <olegblanutsa@gmail.com>
2022-12-06 01:18:03 +0300

    basic setup

--- main_app/views.py
+++ main_app/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request, "main_app/home_page.html")
+
+
+def register_page(request):
+    return render(request, "main_app/register_page.html")
+    
