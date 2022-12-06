e625e9966404f1c9bb0f7c629452025443965ee2
boa00 <olegblanutsa@gmail.com>
2022-12-06 23:43:25 +0300

    custom user manager

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings 
+from django.contrib.auth.models import (
+	AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    
+    def create_user(self, email, password):
+        if email is None:
+            raise TypeError("User must have an enail")
+
+        if password is None:
+            raise TypeError("User must have a password")
+
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, password):
+
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
+from .forms import NewUserForm
+
 # Create your views here.
 def home_page(request):
     return render(request, "main_app/home_page.html")
 
 
 def register_page(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
     return render(request, "main_app/register_page.html")
-    
+
+
+def login_page(request):
+    return render(request, "main_app/login_page.html")
