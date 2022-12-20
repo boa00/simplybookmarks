8cccb93772ed9a4ac5cc43fd11a97ce784dccc76
boa00 <olegblanutsa@gmail.com>
2022-12-20 22:07:15 +0300

    refactoring

--- main_app/google.py
+++ main_app/google.py
@@ -1,7 +1,6 @@
 import requests
 import os 
 import hashlib
-import json 
 import urllib.parse
 
 import jwt
@@ -9,30 +8,19 @@ import jwt
 
 class OpenIDConnectHandler:
 
+    # add constants to separate config file
     oauth_endpoint = "https://accounts.google.com/o/oauth2/v2/auth?"
     toeken_endpoint = "https://oauth2.googleapis.com/token"
     response_type = "code"
     scope = "openid email profile"
     redirect_uri = "http://127.0.0.1:1337/google_openid"
+    grant_type = "authorization_code"
 
     def __init__(self) -> None:
         self.state = hashlib.sha256(os.urandom(1024)).hexdigest()
         self.nonce = hashlib.sha256(os.urandom(1024)).hexdigest()
-
-    # move two methods below to the env variables
-    @property
-    def client_id(self) -> str:
-        secrets_json = open('main_app/openid_secrets.json')
-        client_id = json.load(secrets_json)["web"]["client_id"]
-        secrets_json.close()
-        return client_id
-
-    @property
-    def client_secret(self) -> str:
-        secrets_json = open('main_app/openid_secrets.json')
-        client_id = json.load(secrets_json)["web"]["client_secret"]
-        secrets_json.close()
-        return client_id
+        self.client_id = os.environ['GOOGLE_CLEINT_ID']
+        self.client_secret = os.environ['GOOGLE_CLIENT_SECRET']
 
     def generate_openid_link(self) -> str:
 
@@ -54,7 +42,7 @@ class OpenIDConnectHandler:
             "client_id": self.client_id,
             "client_secret": self.client_secret,
             "redirect_uri": self.redirect_uri,
-            "grant_type": "authorization_code"
+            "grant_type": self.grant_type,
         }
 
         response = requests.post(self.toeken_endpoint, params=request_params)
@@ -66,31 +54,10 @@ class OpenIDConnectHandler:
         token_id = response_data["id_token"]
         all_user_data = jwt.decode(token_id, options={"verify_signature": False})
         user_data = {
-            "email": all_user_data["email"],
-            "name": all_user_data["given_name"]
+            "email": all_user_data.get("email", None),
+            "name": all_user_data.get("given_name", None),
         }
         return user_data
 
-def generate_openid_link():
-    secrets_json = open('main_app/openid_secrets.json')
-    secrets = json.load(secrets_json)["web"]
-    secrets_json.close()
-
-    openid_params = {
-        "client_id": secrets["client_id"],
-        "response_type": "code",
-        "scope": "openid email profile",
-        "redirect_uri": "http://127.0.0.1:1337/google_openid",
-        "state": hashlib.sha256(os.urandom(1024)).hexdigest(),
-        "nonce": hashlib.sha256(os.urandom(1024)).hexdigest(),
-        #"hint": "olegblanutsa@gmail.com",
-    }
-
-    oauth_endpoint = "https://accounts.google.com/o/oauth2/v2/auth?"
-    url = oauth_endpoint + urllib.parse.urlencode(openid_params)
-    return url
-
 handler = OpenIDConnectHandler()
-import os
-print(os.environ['HELLO'])
 print(handler.generate_openid_link())
